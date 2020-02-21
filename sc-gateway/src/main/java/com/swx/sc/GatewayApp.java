93b8782a670ee52320c7bd95d769a99aea5d5f51
shiwenxiang <shiwenxiang@pmlabs.com.cn>
2020-02-21 18:03:53 +0800

    feat(gateway 模块):

---
+++ sc-gateway/src/main/java/com/swx/sc/GatewayApp.java
@@ -0,0 +1,17 @@
+package com.swx.sc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shiwenxiang
+ * @date 2020/2/21
+ */
+@SpringBootApplication
+public class GatewayApp {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApp.class, args);
+    }
+
+
+}
